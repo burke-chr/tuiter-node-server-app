49487e2554b4b6da262fe6cb8272a361cc85df7b
burke-chr <burke.chr@northeastern.edu>
2022-11-25 14:03:22 -0500

    a9 Mongo fa22

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ import UserController
   from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import cors from 'cors';
+import mongoose from 'mongoose';
 
+const CONNECTION_STRING = process.env.WEBDEV_DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors())
 app.use(express.json());
